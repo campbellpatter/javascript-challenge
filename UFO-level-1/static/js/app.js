60eb44d28e57b2e0161d8978238ddb1bb35082d7
Robert <robert.patterson@gatech.edu>
2019-11-29 16:08:46 -0500

    adding finished js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,43 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+d3.select("#filter-btn").on("click",runEnter);
+d3.select('form').on('submit',runEnter);
+
+function runEnter() {
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+  var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+  console.log(filteredData);
+
+    // create variable for tbody element
+    var mybody = d3.select("tbody");
+
+    // remove any exisitng data
+    mybody.html("");
+
+  filteredData.forEach(function(ufoSighting) {
+    console.log(ufoSighting);
+    var row = mybody.append("tr");
+
+    Object.entries(ufoSighting).forEach(function([key, value]) {
+      console.log(key, value);
+      // Append a cell to the row for each value
+      // in the ufoSighting object
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+};
+
+
+// Use D3 to select the table
+var table = d3.select("table");
+
+// Use d3 to create a bootstrap striped table
+// http://getbootstrap.com/docs/3.3/css/#tables-striped
+table.attr("class", "table table-hover table-striped");
